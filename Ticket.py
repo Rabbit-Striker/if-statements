2b13bce946169b9c303c8f17d4f7756d5490adb1
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-13 11:49:45 +0200

    Added age input

---
+++ Ticket.py
@@ -0,0 +1,17 @@
+
+print("The ticket price is determined based on customer's age, presenting a proof of age is obligatory at entrance. ")
+15
+age = int(input("How old are you?: "))
+
+if age >= 40:
+    print("Your ticket price is: 10$")
+elif age >= 18:
+    print("Your ticket price is: 12$")
+elif age >= 10:
+    print("Your ticket price is: 8$")
+elif age >= 4:
+    print("Your ticket price is: 5$")
+else:
+    print("Sorry, by our regulations, children under 4 years are not allowed.")
+
+print("Thank you for choosing us.")
