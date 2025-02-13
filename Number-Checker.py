828a96433f6532aee25ac3f311beb81da596295b
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-13 12:16:38 +0200

    Added input and welcoming

---
+++ Number-Checker.py
@@ -0,0 +1,7 @@
+
+print("Hello, we are here to help you know if the entered number is odd or even")
+
+num = int(input("Enter a number: "))
+
+if num/2 == int(num/2):
+    print("The number is even")
