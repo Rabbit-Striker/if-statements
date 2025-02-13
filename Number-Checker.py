21f1987dc0673024c1da5a6bd2be5c383ad273db
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-13 13:03:43 +0200

    Added comments

--- Number-Checker.py
+++ Number-Checker.py
@@ -1,11 +1,17 @@
-
+# Print a welcome message
 print("Hello, we are here to help you know if the entered number is odd or even")
+
+# Prompt the user to enter a number
 num = float(input("Enter a number: "))
+
+# Check if the entered number is a whole number
 if num != int(num):
     print("please enter a whole number")
 else:
     num = int(num)
-    if num/2 == int(num/2):
+    # Check if the number is even
+    if num / 2 == int(num / 2):
         print("The number is even")
     else:
+        # If the number is not even, it is odd
         print("The number is odd")
