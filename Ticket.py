2b7ee722ead4e7c08afbeb8da98be2c7068cec3e
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-13 13:04:52 +0200

    Added comments

--- Ticket.py
+++ Ticket.py
@@ -1,8 +1,13 @@
+# Welcome message
 print("Welcome to Cosmic Cinemas!!!")
+
+# Information about ticket pricing
 print("The ticket price is determined based on customer's age, presenting a proof of age is obligatory at entrance. ")
 
+# Get the age of the customer
 age = int(input("How old are you?: "))
 
+# Determine the ticket price based on age
 if age >= 40:
     print("Your ticket price is: 10$")
 elif age >= 18:
@@ -14,4 +19,5 @@ elif age >= 4:
 else:
     print("Sorry, by our regulations, children under 4 years are not allowed.")
 
+# Thank you message
 print("Thank you for choosing us.")
