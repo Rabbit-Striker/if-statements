5901bd9d9d5ea57cf5aeedf3d86337ea73652570
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-13 13:11:38 +0200

    Nested if conditions

--- Login.py
+++ Login.py
@@ -7,3 +7,14 @@ user = input("Enter username: ")
 
 # Ask the user for a password
 password = input("Enter password: ")
+
+# Check if the username and password are correct
+if user == "admin":
+    # Check if the password is correct
+    if password == "1234":
+        print("Access granted")
+    else:
+        print("Access denied")
+
+else:
+    print("Access denied")
