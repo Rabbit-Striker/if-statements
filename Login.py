ec2229d1bb171231b5dc88a60e7a2b664b99bb2e
Rabbit-Striker <husseinsharafeddein@gmail.com>
2025-02-13 13:08:45 +0200

    implemented basic input

--- Login.py
+++ Login.py
@@ -2,3 +2,8 @@
 
 # Display a welcoming message
 print("Welcome to the Simple Login System")
+# Ask the user for a username
+user = input("Enter username: ")
+
+# Ask the user for a password
+password = input("Enter password: ")
